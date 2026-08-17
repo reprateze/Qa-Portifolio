a827cb7d6d72c93e59d9c68b622de48e5af5da3f
reprateze <reprateze@gmail.com>
2026-08-17 11:04:40 -0300

    test: add cart checkout tests

--- tests/ui/test_cart.py
+++ tests/ui/test_cart.py
@@ -1,23 +1,66 @@
 import pytest
 
-from pages.login_page import LoginPage
-from pages.inventory_page import InventoryPage
-from pages.cart_page import CartPage
-
-
 pytestmark = pytest.mark.ui
+
 
 class TestCart:
 
-    def test_added_product_visibility_in_cart(self, page):
-        login_page = LoginPage(page)
-        Inventory_page = InventoryPage(page)
-        cart_page = CartPage(page)
-
-        login_page.login("standard_user", "secret_sauce")
-
-        Inventory_page.add_backpack_to_cart()
-        Inventory_page.open_cart()
+    def test_added_product_visibility_in_cart(self, logged_in_inventory_page, cart_page):
+        logged_in_inventory_page.add_backpack_to_cart()
+        logged_in_inventory_page.open_cart()
 
         assert cart_page.get_product_name() == "Sauce Labs Backpack"
 
+    def test_multiplos_produtos_no_carrinho(self, logged_in_inventory_page, cart_page):
+        logged_in_inventory_page.add_backpack_to_cart()
+        logged_in_inventory_page.add_bike_light_to_cart()
+        logged_in_inventory_page.add_bolt_t_shirt_to_cart()
+
+        logged_in_inventory_page.open_cart()
+
+        products = cart_page.get_product_names()
+
+        expected_products = [
+            "Sauce Labs Backpack",
+            "Sauce Labs Bike Light",
+            "Sauce Labs Bolt T-Shirt",
+        ]
+        assert products == expected_products
+
+    def test_remover_backpack(self, logged_in_inventory_page, cart_page):
+        logged_in_inventory_page.add_backpack_to_cart()
+        logged_in_inventory_page.add_bike_light_to_cart()
+        logged_in_inventory_page.add_bolt_t_shirt_to_cart()
+
+        logged_in_inventory_page.open_cart()
+
+        cart_page.remove_product("sauce-labs-backpack")
+
+        assert not cart_page.is_backpack_visible()
+
+    def test_remover_produto_mantem_outros_no_carrinho(self, logged_in_inventory_page, cart_page):
+        logged_in_inventory_page.add_backpack_to_cart()
+        logged_in_inventory_page.add_bike_light_to_cart()
+        logged_in_inventory_page.open_cart()
+
+        cart_page.remove_product("sauce-labs-backpack")
+
+        assert cart_page.get_product_names() == ["Sauce Labs Bike Light"]
+
+    def test_empty_cart(self, logged_in_inventory_page, cart_page):
+        logged_in_inventory_page.open_cart()
+
+        assert cart_page.get_product_names() == []
+
+    def test_continue_Shopping(self, logged_in_inventory_page, cart_page):
+        logged_in_inventory_page.open_cart()
+        cart_page.continue_shopping()
+
+        assert logged_in_inventory_page.is_inventory_visible()
+
+    def test_go_to_checkout(self, logged_in_inventory_page, cart_page, page):
+        logged_in_inventory_page.add_backpack_to_cart()
+        logged_in_inventory_page.open_cart()
+        cart_page.go_to_Checkout()
+
+        assert "checkout-step-one" in page.url
